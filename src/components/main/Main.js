848187c7d1de578e632125d3be9c48ce270fb2e1
cdmackbook <vd>
2016-11-05 18:55:35 +1100

    created store with todo CRUD actions

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,29 +1,43 @@
 import React, { Component } from 'react';
 import TodoList from  '../todo-list/todo-list';
-
-const todos =[{
-	task :  'Complete React tutorial',
-	isCompleted : false 
-},
-{
-	task :  'Create  a  react app',
-	isCompleted : false 
-}]
-
+import * as Actions from  '../todo-list/actions';
+import TodoStore from  '../../store/TodoStore';
+import _ from  'lodash';
 
 export  default class Main extends Component {
 	constructor()
 	{
 		super();
 		this.state = {
-			todos
+			todos : TodoStore.getAll()
 		};
+	}
+	componentWillMount(){
+		TodoStore.on("change" , () => {
+			this.setState({
+				todos : TodoStore.getAll()
+			})
+		})
 	}
 	render(){
 		return(
 			<div>
-				<TodoList  todos={ this.state.todos }/>
+				<TodoList  
+					todos={ this.state.todos }
+					createTask={ this.createTask.bind(this) }
+					toggleEditMode={ Actions.toggleEditMode.bind(this) }
+					toggleTask={ Actions.toggleTask.bind(this) }
+					saveTask={ Actions.saveTask.bind(this) }
+					deleteTask={ Actions.deleteTask.bind(this) }
+				/>
 			</div>	
 		);
+	}
+
+	createTask(task){
+		const foundTodo = _.find(this.state.todos, todo => todo.task === task);
+		if(foundTodo) return;
+
+		Actions.createTask(task);
 	}
 }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
 import ListHeader from  './list-header';
 import ListItems from  './list-items';
+import CrearteItem from  './create-item';
 import _ from  'lodash';
 
 export  default class Todolist extends Component {
 	renderItems() {
 		const todos = this.props.todos;
+		const props = _.omit(this.props, 'todos');
 		return (
-			_.map(todos, ( todo , index ) => <ListItems key={ index }  {...todo} />)
+			_.map(todos, ( todo , index ) => <ListItems key={ index }  {...todo} {...props} />)
 		);
 	}
 	render(){
+		const createItemStyle = { 
+			marginTop : 20
+		}
 		return(
-			<table> 
-				<ListHeader />		
-				{ this.renderItems() } 		
-			</table>
+			<div>
+				<CrearteItem createTask={ this.props.createTask }/> 
+				<table style={ createItemStyle }> 
+					<ListHeader />		
+					<tbody>
+						{ this.renderItems() } 		
+					</tbody>
+				</table>
+			</div>
 		);
 	}
 }
